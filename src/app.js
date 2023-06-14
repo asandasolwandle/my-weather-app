e124a5a07cadbb692c1bc330e76f1febd526ea97
asandasolwandle <135405518+asandasolwandle@users.noreply.github.com>
2023-06-14 13:38:14 +0200

    Added relevant icon from API

--- src/app.js
+++ src/app.js
@@ -27,15 +27,21 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.time * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+  );
+  iconElement.setAttribute("alt", response.data.condition.icon);
 }
 let apiKey = "9e21abf3913eb0t4o03834ad4082bcb4";
-let apiUrl =
-  "https://api.shecodes.io/weather/v1/current?query=Johannesburg&key=9e21abf3913eb0t4o03834ad4082bcb4&units=metric";
+let city = "Johannesburg";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=9e21abf3913eb0t4o03834ad4082bcb4&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
